1e1d02b49de64a64c7ba62b86f52fe474783128a
Yatin Burhmi <yatin.burhmi@kuliza.com>
2020-09-11 11:41:04 +0530

    Added: Logo in Header Component

--- src/Components/Header_footer/Header.js
+++ src/Components/Header_footer/Header.js
@@ -4,6 +4,7 @@ import Toolbar from '@material-ui/core/Toolbar'
 import Button from '@material-ui/core/Button'
 
 import {Link} from 'react-router-dom'
+import {CityLogo} from '../ui/icons';
 
 export default class Header extends Component {
     render() {
@@ -22,7 +23,12 @@ export default class Header extends Component {
                 >
                     <div style={{flexGrow:1}}>
                         <div className="header_logo">
-                            LOGO
+                            <CityLogo 
+                                link={true} 
+                                linkTo="/" 
+                                width="70px"
+                                height="70px"
+                            />
                         </div>
                     </div> 
 
