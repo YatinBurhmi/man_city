fb4e688f16d9fdb0aff84fc2bad4f0431d16980c
Yatin Burhmi <yatin.burhmi@kuliza.com>
2020-09-11 11:19:20 +0530

    Added: Header

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from './Hoc/Layout';
+
+
+const Routes = (props) => {
+  return(
+    <Layout>
+      second Line
+    </Layout>
+  )
+}
+
+export default Routes;
+
